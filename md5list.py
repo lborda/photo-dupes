f0038edd2132e70189182f38ff6d20a79f3f60df
Leonardo Borda <leonardoborda@gmail.com>
2014-10-31 17:58:20 -0400

    add multiple folders in the args

--- md5list.py
+++ md5list.py
@@ -1,5 +1,5 @@
 # encoding=utf-8
-import os, hashlib
+import os, sys, hashlib
 
 def get_file_descriptor(filepath, mode='rb'):
     fd = None
@@ -27,32 +27,33 @@ def md5_list(dir):
     tot_repeated = 0
     tot_bytes = 0
     hash_list = {}
-    try:
-        for root, dirs, files in os.walk(dir):
-            for file in files:
-                    filepath = os.path.join(root, file)
-                    if get_file_descriptor(filepath):
-                        fd = get_file_descriptor(filepath)
-                        hash = get_md5_for_file(fd, 8192)
-                        if hash not in hash_list:
-                            hash_list[hash] = filepath
-                        else:
-                            print "{0} ; {1} ; {2}; {3}".format(hash, filepath, hash_list[hash], os.path.getsize(filepath))
-                            tot_repeated += 1
-                            tot_bytes += os.path.getsize(filepath)
-    except KeyboardInterrupt:
-        print "You cancelled!"
-    finally:
-        print "Total of dup files: {}".format(tot_repeated)
-        print "Total size of duplicated files: {} Mbytes".format(bytestomegabytes(tot_bytes))
-            #print hash_list
+    for d in dir:
+        try:
+            for root, dirs, files in os.walk(d):
+                for file in files:
+                        filepath = os.path.join(root, file)
+                        if get_file_descriptor(filepath):
+                            fd = get_file_descriptor(filepath)
+                            hash = get_md5_for_file(fd, 8192)
+                            if hash not in hash_list:
+                                hash_list[hash] = filepath
+                            else:
+                                print "{0} ; {1} ; {2}; {3}".format(hash, filepath, hash_list[hash], os.path.getsize(filepath))
+                                tot_repeated += 1
+                                tot_bytes += os.path.getsize(filepath)
+        except KeyboardInterrupt:
+            print "You cancelled!"
+    print "Total of duplicated files: {}".format(tot_repeated)
+    print "Total size of duplicated files: {} Mbytes".format(bytestomegabytes(tot_bytes))
 
 def add_duplicate(hash, filename):
     pass
 
-def run():
+def run(arg):
     #md5_list('/home/lborda/Pictures')
-    md5_list('/media/lborda/data')
+    md5_list(arg)
+    print "Top directories searched: {0} ".format(arg)
 
 if __name__ == '__main__':
-    run()
+        run(sys.argv[1:])
+
