d7aa603f244e28dcba7a6fd539ec53de13b073a8
Leonardo Borda <leonardoborda@gmail.com>
2014-10-31 12:02:59 -0400

    fixing when a directory is a broken link

--- md5list.py
+++ md5list.py
@@ -1,16 +1,27 @@
 # encoding=utf-8
 import os, hashlib
-import hurry.filesize
 
-def get_md5_for_file(filepath, block_size=2**20):
+def get_file_descriptor(filepath, mode='rb'):
+    fd = None
+    try:
+        fd = open(filepath, mode)
+        return fd
+    except IOError:
+        return fd
+
+def get_md5_for_file(fd, block_size=2**20):
     md5 = hashlib.md5()
-    filepath = open(filepath, 'rb')
     while True:
-        data = filepath.read(block_size)
+        data = fd.read(block_size)
         if not data:
             break
         md5.update(data)
+    fd.close
     return md5.hexdigest()
+
+
+def bytestomegabytes(bytes):
+    return (bytes / 1024) / 1024
 
 def md5_list(dir):
     tot_repeated = 0
@@ -19,26 +30,29 @@ def md5_list(dir):
     try:
         for root, dirs, files in os.walk(dir):
             for file in files:
-                filepath = os.path.join(root, file)
-                hash = get_md5_for_file(filepath, 8192)
-                if hash not in hash_list:
-                    hash_list[hash] = filepath
-                else:
-                    print "{0} ; {1} ; {2}; {3}".format(hash, filepath, hash_list[hash], os.path.getsize(filepath))
-                    tot_repeated += 1
-                    tot_bytes += os.path.getsize(filepath)
+                    filepath = os.path.join(root, file)
+                    if get_file_descriptor(filepath):
+                        fd = get_file_descriptor(filepath)
+                        hash = get_md5_for_file(fd, 8192)
+                        if hash not in hash_list:
+                            hash_list[hash] = filepath
+                        else:
+                            print "{0} ; {1} ; {2}; {3}".format(hash, filepath, hash_list[hash], os.path.getsize(filepath))
+                            tot_repeated += 1
+                            tot_bytes += os.path.getsize(filepath)
     except KeyboardInterrupt:
         print "You cancelled!"
     finally:
-        print "Total of repeated files: {}".format(tot_repeated)
-        print "Total size of duplicated files: {}".format(hurry.filesize.size(tot_bytes))
+        print "Total of dup files: {}".format(tot_repeated)
+        print "Total size of duplicated files: {} Mbytes".format(bytestomegabytes(tot_bytes))
             #print hash_list
 
 def add_duplicate(hash, filename):
     pass
 
 def run():
-    md5_list('/home/lborda/dev')
+    #md5_list('/home/lborda/Pictures')
+    md5_list('/media/lborda/data')
 
 if __name__ == '__main__':
     run()
